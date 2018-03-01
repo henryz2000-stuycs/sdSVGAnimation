a21f4b9a2edcb0bcb4d3564e890800bd4ff7adbb
henryz2000 <hzheng3@stuy.edu>
2018-03-01 14:06:52 -0500

    dvd doesnt work

---
+++ svg.js
@@ -0,0 +1,98 @@
+var clear = document.getElementById("clear");
+var stop = document.getElementById("stop");
+var circle = document.getElementById("circle");
+var dvd = document.getElementById("dvd");
+var svg = document.getElementById("svg");
+
+var id;
+
+//for circle
+var size, grow;
+//size = 1;
+//grow = 1;
+
+//for dvd
+var diffX, diffY;
+
+var clearCallback = function(e){
+    svg.innerHTML = "";
+};
+
+var stopCallback = function(e){
+    clearInterval(id);
+};
+
+var circleCallback = function(e){
+    size = 1;
+    grow = 1;
+    stopCallback();
+    
+    var run = function(){
+	clearCallback();
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute("cx", parseInt(svg.getAttribute("width"))/2);
+	c.setAttribute("cy", parseInt(svg.getAttribute("height"))/2);
+	c.setAttribute("r", size);
+	svg.appendChild(c);
+	if (size == (parseInt(svg.getAttribute("width"))+parseInt(svg.getAttribute("height")))/4){
+	    grow = 0;
+	}
+	else if (size == 0){
+	    grow = 1;
+	}
+	if (grow == 1){
+	    size++;
+	}
+	else{
+	    size--;
+	}
+    };
+    id = setInterval(run, 10);
+};
+
+var dvdCallback = function(e){
+    var x = parseInt(svg.getAttribute("width"))/2
+    var y = parseInt(svg.getAttribute("height"))/2;
+    diffX = 2;
+    diffY = 1;
+    
+    stopCallback();
+    
+    var run = function(){
+	clearCallback();
+	/*
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+	c.setAttribute("cx", x);
+	c.setAttribute("cy", y);
+	c.setAttribute("r", 10);
+	svg.appendChild(c);
+	*/
+	var c = document.createElementNS("http://www.w3.org/2000/svg", "image");
+	c.setAttribute("xlink:href", "https://en.wikipedia.org/wiki/DVD#/media/File:DVD_logo.svg");
+	c.setAttribute("x", x)
+	c.setAttribute("y", y);
+	c.setAttribute("height", 10);
+	c.setAttribute("width", 10);
+	svg.appendChild(c);
+	if (x <= 0){
+	    diffX = 2;
+	}
+	else if (x >= parseInt(svg.getAttribute("width"))){
+	    diffX = -2;
+	}
+	if (y <= 0){
+	    diffY = 1;
+	}
+	else if (y >= parseInt(svg.getAttribute("height"))){
+	    diffY = -1;
+	}
+	x += diffX;
+	y += diffY;
+    };
+    id = setInterval(run, 10);
+};
+
+clear.addEventListener("click", clearCallback);
+stop.addEventListener("click", stopCallback);
+circle.addEventListener("click", circleCallback);
+dvd.addEventListener("click", dvdCallback);
